d7f9ec9cf25187bc39941f54d261cbdafc5f5f84
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2021-05-17 13:00:02 +0200

    finalized tests

--- tests/alllines.test.js
+++ tests/alllines.test.js
@@ -1,27 +1,35 @@
 var rl = require('../index.js');
 
-test('read all lines', function(done) {
-  function callback(err, res) {
-    expect(res).toBe({
-      all: "AAAA\r\nBBBB\r\nCCCC\r\n",
-      row: {"1": "AAAA", "2": "BBBB", "3": "CCCC"}
-    });
-    expect(err).toBe(null);
-    done();
-  }
+var testFilePath = './tests/file.txt';
 
-  rl.alllines('./tests/file.txt', callback);
-});
+describe('alllines()', function() {
 
-test('unavailable file', function(done) {
-  function callback(err, res) {
-    expect(res).toBe({
-      all: '',
-      row: {}
-    });
-    expect(err.message).toBe('ENOENT: no such file or directory, open \'./tests/unavailable.txt\'');
-    done();
-  }
+  test('read all lines', function(done) {
+    function callback(err, res) {
+      expect(res).toStrictEqual({
+        all: "AAAA\r\nBBBB\r\nCCCC\r\n",
+        row: {
+          "1": "AAAA",
+          "2": "BBBB",
+          "3": "CCCC"
+        }
+      });
+      expect(err).toStrictEqual(null);
+      done();
+    }
+    rl.alllines(testFilePath, callback);
+  });
 
-  rl.alllines('./tests/unavailable.txt', callback);
+  test('unavailable file', function(done) {
+    function callback(err, res) {
+      expect(res).toStrictEqual({
+        all: '',
+        row: {}
+      });
+      expect(err.message).toStrictEqual(expect.stringMatching(/^ENOENT: no such file or directory, open '.+unavailable.txt'$/));
+      done();
+    }
+    rl.alllines('./tests/unavailable.txt', callback);
+  });
+
 });
--- tests/multiline.test.js
+++ tests/multiline.test.js
@@ -2,87 +2,72 @@ var rl = require('../index.js');
 
 var testFilePath = './tests/file.txt';
 
-describe('oneline()', function() {
+describe('multilines()', function() {
 
   test('read first line', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('AAAA');
+      expect(res).toStrictEqual({"1": "AAAA"});
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, 1, callback);
+    rl.multilines(testFilePath, [1], callback);
   });
 
   test('read last line', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('CCCC');
+      expect(res).toStrictEqual({"3": "CCCC"});
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, 3, callback);
+    rl.multilines(testFilePath, [3], callback);
   });
 
-  test('read line after last line', function(done) {
+  test('read first and last line', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('');
+      expect(res).toStrictEqual({
+        "1": "AAAA",
+        "3": "CCCC"
+      });
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, 4, callback);
-  });
-
-  test('read line 0', function(done) {
-    function callback(err, res) {
-      expect(res).toStrictEqual('');
-      expect(err).toStrictEqual(null);
-      done();
-    }
-    rl.oneline(testFilePath, 0, callback);
-  });
-
-  test('read line -1', function(done) {
-    function callback(err, res) {
-      expect(res).toStrictEqual('');
-      expect(err).toStrictEqual(null);
-      done();
-    }
-    rl.oneline(testFilePath, -1, callback);
+    rl.multilines(testFilePath, [1, 3], callback);
   });
 
   test('read unavailable line', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('');
+      expect(res).toStrictEqual({});
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, 100, callback);
+    rl.multilines(testFilePath, [100], callback);
   });
 
   test('invalid line format (letter)', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('');
+      expect(res).toStrictEqual({});
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, 'a', callback);
+    rl.multilines(testFilePath, ['a'], callback);
   });
 
   test('invalid line format (array)', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('');
+      expect(res).toStrictEqual({});
       expect(err).toStrictEqual(null);
       done();
     }
-    rl.oneline(testFilePath, ['a'], callback);
+    rl.multilines(testFilePath, [[]], callback);
   });
 
   test('unavailable file', function(done) {
     function callback(err, res) {
-      expect(res).toStrictEqual('');
+      expect(res).toStrictEqual({});
       expect(err.message).toStrictEqual(expect.stringMatching(/^ENOENT: no such file or directory, open '.+unavailable.txt'$/));
       done();
     }
-    rl.oneline('./tests/unavailable.txt', 1, callback);
+    rl.multilines('./tests/unavailable.txt', [1], callback);
   });
 
 });
